0cb72cc08c807bb82980a6f9a0baf8c13aa0764c
Fidel97 <130863403+Fidel97@users.noreply.github.com>
2023-05-18 23:51:17 -0500

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,55 +1,67 @@
-#dependicies 
-import os 
+#dependicies
+import os
 import csv
-#Files to Load
-budget_csv = os.path.join( 'Resources', 'budget_data.csv')
-months =[]
-change_list= []
-pl=[]
-with open(budget_csv, 'r') as csvfile:
+#Files to upload
+election_csv= os.path.join('Resources/election_data.csv')
+total_vote_count= 0
+break_line = "------------------------------"
+candidate_votes= {}
+
+
+with open(election_csv, 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    
     header = next(csvreader)
+    # first_row= next(csvreader)  
+    # # first_row[2] = "cand"
+    # # insert key value pair
+    # if "cand" not in candidate_votes:
+    #     candidate_votes["cand"] = 1
+    # else:
+    #     candidate_votes["cand"] += 1
 
-    first_row= next(csvreader)
-    months.append(first_row[0])
-    pl.append(int(first_row[1]))
-    previous_value= int(first_row[1])
+    # # increment key value pair
+    # # candidate_votes["cand"] += 1
+    for row in csvreader: 
+        # get the candidate name   
+        candidate_name = row[2]    
+        # this should increase for each row
+        total_vote_count += 1 
+        # check if the candidate is in the candidate list
+        # if yes, then increase the candidate's vote
+        if candidate_name not in candidate_votes:
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name] += 1 
 
-    # Loop through the data
-    for row in csvreader:
-        
-        months.append(row[0])
-        pl.append(int(row[1]))
-        change= int(row[1])-previous_value
-        change_list.append(change)
-        previous_value=int(row[1])
+        total_vote_count= sum(candidate_votes.values())
+candidate_votes["Charles Percent"] = round((candidate_votes["Charles Casper Stockham"]/total_vote_count) * 100, 3)
+candidate_votes["Diana Percent"] = round((candidate_votes["Diana DeGette"]/total_vote_count) * 100, 3)
+candidate_votes["Raymon Percent"] = round((candidate_votes["Raymon Anthony Doane"]/total_vote_count) * 100, 3)
 
-
-
-avg_change= round(sum(change_list)/len(change_list),2)
-
-max_increase=max(change_list)
-min_increase=min(change_list)
-
-max_index=change_list.index(max_increase)
-min_index=change_list.index(min_increase)
-Max_month=months[max_index+1]
-Min_month=months[min_index+1]
+winner=max(candidate_votes, key=candidate_votes.get)
 
 
-budget_output= (
-f'Financial Analysis\n'
-f'----------------------------\n'
-f'Total Months: {len(months)}\n'
-f'Total: ${sum(pl)}\n'
-f'Average Change: ${avg_change}\n'
-f'Greatest Increase in Profits: {Max_month} (${max_increase})\n'
-f'Greatest Decrease in Profits: {Min_month} (${min_increase})\n')
+print("Election Results")
+print(break_line)
+print("Total Vote: " + str(total_vote_count))
+print("Charles Casper Stockham: " + str(candidate_votes["Charles Percent"]) + "% " + str(candidate_votes["Charles Casper Stockham"]))
+print("Diana DeGette: " + str(candidate_votes["Diana Percent"]) + "% " + str(candidate_votes["Diana DeGette"]))
+print("Raymon Anthony Doane: " + str(candidate_votes["Raymon Percent"]) + "% " + str(candidate_votes["Raymon Anthony Doane"]))
+print(break_line)
+print("Winner: " + str(winner))
 
-print(budget_output)
-out_puttext = os.path.join( 'Analysis', 'budget_analysis.txt')
-with open(out_puttext, 'w') as textfile:
-    textfile.write(budget_output)
+output_result = os.path.join(".", "analysis", "result.txt")
+
+with open(output_result, "w") as txt_file:
+    txt_file.write("Election Results" + "\n")
+    txt_file.write(break_line + "\n")
+    txt_file.write("Total Vote: " + str(total_vote_count) + "\n")
+    txt_file.write(break_line + "\n")
+    txt_file.write("Charles Casper Stockham: " + str(candidate_votes["Charles Percent"]) + "% " + str(candidate_votes["Charles Casper Stockham"]) + "\n") 
+    txt_file.write("Diana DeGette: " + str(candidate_votes["Diana Percent"]) + "% " + str(candidate_votes["Diana DeGette"]) + "\n")
+    txt_file.write("Raymon Anthony Doane: " + str(candidate_votes["Raymon Percent"]) + "% " + str(candidate_votes["Raymon Anthony Doane"]) + "\n")
+    txt_file.write(break_line + "\n")
+    txt_file.write("Winner: " + str(winner) + "\n")
+    txt_file.write(break_line + "\n")
